c8d9f5b93a6d0dc017eeef23ab3ff30acca28823
devDudu <edufernandes.1895@gmail.com>
2023-11-07 18:12:13 -0300

    Cadastrando contatos (create)

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -4,20 +4,26 @@ exports.middlewareGlobal = (req, res, next) => {
   res.locals.user = req.session.user;
   next();
 };
-
 exports.outroMiddleware = (req, res, next) => {
   next();
 };
-
 exports.checkCsrfError = (err, req, res, next) => {
   if (err) {
     return res.render('404');
   }
-
+  next();
+};
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
   next();
 };
 
-exports.csrfMiddleware = (req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
   next();
 };
