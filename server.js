8351009e4a03723a012f29e81d2276d1a02a0376
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-02-13 01:23:50 -0300

    fix express port reference

--- server.js
+++ server.js
@@ -1,42 +1,51 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.emit('pronto');
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch(e => console.log(e));
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const routes = require('./routes');
-const path = require('path');
-const helmet = require('helmet');
-const csrf = require('csurf');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const routes = require("./routes");
+const path = require("path");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
 
 app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  secret: "akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()",
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
+    httpOnly: true,
+  },
 });
 app.use(sessionOptions);
 app.use(flash());
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
 app.use(csrf());
 // Nossos próprios middlewares
@@ -44,9 +53,7 @@ app.use(middlewareGlobal);
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
-const port = process.env.APP_PORT
-app.on('pronto', () => {
+const port = process.env.APP_PORT;
+app.on("pronto", () => {
   app.listen(port);
 });
-
-
